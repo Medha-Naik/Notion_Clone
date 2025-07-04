5d2cdcc3d2916439ca048576ca2683db066d826c
Medhaa-Naik <medhaa.m.n@gmail.com>
2025-07-04 19:47:51 +0530

    Add basic flask structure with templates and static folders

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Tzenz! Your Notion clone journey starts here."
+    return("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
